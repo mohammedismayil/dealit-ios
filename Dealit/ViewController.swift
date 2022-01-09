0588abf07f6b92ca3678576a93c5d0ca8d7f8d48
mohammedismayil <mohammedismayil10@gmail.com>
2022-01-09 19:16:26 +0530

    Initial Commit

---
+++ Dealit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dealit
+//
+//  Created by Mohammed Ismayil on 09/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
